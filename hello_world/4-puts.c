a8f1e8438c0ddd27b57ca18192046b9ba8461bc1
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-19 14:51:14 -0400

    6 Size

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -11,3 +11,44 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
+
+
+
+
+
+#include <stdio.h>
+
+/**
+* main - Entry point 
+*
+* Return: Always 0
+*/
+int main (void)
+{
+        printf("Size of a char: %lu byte(s)", sizeof(char));
+        printf("Size of an int: %lu byte(s)", sizeof(int));
+        printf("Size of a long int: %lu byte(s)", sizeof(long int));
+        printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+        printf("Size of a float: %lu byte(s)", sizeof(float));
+        return (0);
+
+}
+
+#include <stdio.h>
+
+/**
+* main - Entry point 
+*
+* Return: Always 0
+*/
+int main (void)
+{
+        printf("Size of a char: %lu byte(s)", sizeof(char));
+        printf("Size of an int: %lu byte(s)", sizeof(int));
+        printf("Size of a long int: %lu byte(s)", sizeof(long int));
+        printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+        printf("Size of a float: %lu byte(s)", sizeof(float));
+        return (0);
+
+}
