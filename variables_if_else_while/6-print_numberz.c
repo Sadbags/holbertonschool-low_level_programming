2ba3b6cbdd220ad7a74fbb6a92d0ff767cca404a
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 13:50:20 -0400

    6 print numbers

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main - Print all single digit numbers of base 10 
  * Description: Print all single digit numbers of base 10 starting from 0 
  * Return: Always 0
-*/
+ */
 int main(void)
 {
 int n;
