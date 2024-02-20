da3efe5b0ee6d9e43e4c51b3733bbcd501ac23cc
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-20 15:48:16 -0400

    9 print combination2

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description: Print all possible combinations fo single-digit numbers.
+ * Numbers must be separated by commas and a space.
+ * You can only use `putchar` to print to console
+ * You can only use `putchar` up to four times.
+ * You are not allowed to use any variable of type `char`.
+ *Return: 0
  */
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 48; n < 58; n++)
-putchar(n);
-if (n != 57)
-putchar(44);
-putchar(32);
-putchar(10);
-return (0);
+	for (n = '0' ; n <= '9' ; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
