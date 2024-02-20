e8f99f4ebfbed8c6c66d2e2764e997672ec179f8
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-19 23:07:56 -0400

    6 Size 9

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - main funtion 
  *
- * Return: Always 0
+ * Return: Always 0. (Success)
  */
-
 int main(void)
-
 {
 	int a;
 	long int b;
@@ -22,4 +20,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
-
