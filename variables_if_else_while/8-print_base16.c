79b2d4c7cd3b441f5e559b3de1ae97fa6771d146
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 14:42:33 -0400

    8 print base2

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,16 +2,16 @@
 /**
  * main - Print all the numbers of base 16 in lowercase
  * Return: 0
- */
+*/
 int main(void)
 {
-    int i;
-    char c;
+int n;
+char c;
 
-    for (i = 0; i < 10; i++)
-        putchar(i + '0');
-    for (c = 'a'; c <= 'f'; c++)
-        putchar(c);
-    putchar('\n');
-    return (0);
+for (n = 0 ; n <= 9; n++)
+putchar(n + '0');
+for (c = 'a' ; c <= 'f' ; c++)
+putchar(c);
+putchar('\n');
+return (0);
 }
