6b3fa0f3c96ba9618c16d0c9832239264ed9491c
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 15:00:12 -0400

    9 print combination1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
-for (n = 48 ; n <= 57; n++)
+
+for (n = 48; n < 58; n++)
 putchar(n);
-for (n = 97 ; n <= 102 ; n++)
-putchar(n);
-putchar('\n');
+if (n != 57)
+putchar(44);
+putchar(32);
+putchar(10);
 return (0);
 }
