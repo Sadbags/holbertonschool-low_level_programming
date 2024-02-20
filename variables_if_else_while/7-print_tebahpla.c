8bb5f54dd49c940a7e6e04c24b22eb3f13f74091
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 14:20:18 -0400

    7 print alphabet4

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - Print the alphabet in lowercase letters in reverse
-*
-* Return: Always 0 (Success)
-*/
+ * main - print lowercase alphabet in reverse
+ * Description: print lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char letter;
+char c;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-	putchar(letter);
-
-	putchar('\n');
-
-	return (0);
+for (c = 'z'; c >= 'a'; c--)
+putchar(c);
+putchar('\n');
+return (0);
 }
