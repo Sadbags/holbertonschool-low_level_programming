a1b5b0e34190beb122270da3ae876144ec06c2b4
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 13:20:17 -0400

    5 print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print singl digit numbers
+ * Description: Print all single digit numbers of base 10 starting from 0
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 10; n++)
+printf("%d", n);
+return (0);
+}
