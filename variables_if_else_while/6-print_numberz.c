7135cd9feb972656a32b74343fd375afba2c0d27
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-20 13:50:57 -0400

    6 print numbers

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Print all single digit numbers of base 10 
- * Description: Print all single digit numbers of base 10 starting from 0 
- * Return: Always 0
- */
+* main - Print all single digit numbers of base 10 
+* Description: Print all single digit numbers of base 10 starting from 0 
+* Return: Always 0
+*/
 int main(void)
 {
 int n;
