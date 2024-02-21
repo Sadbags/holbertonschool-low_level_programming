1252a0fa83d0248e8977f33c6673dd5d1e23f961
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-21 13:02:11 -0400

    2 print alphabet

--- functions_nested_loops/2-print_alphabet.c
+++ functions_nested_loops/2-print_alphabet.c
@@ -1,20 +1,19 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - Entry point
- * Return:0
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
-int i;
 char c;
+int i;
 
 for (i = 0; i < 10; i++)
 {
-c = 'a';
-while (c <= 'z')
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
-c++;
 }
 _putchar('\n');
 }
