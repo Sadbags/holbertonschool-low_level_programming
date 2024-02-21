03b1df1769253f4acb2f191f2c4334661255f2df
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-21 13:31:03 -0400

    4 is alpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
  * _isalpha - checks for alphabetic character
- * @c: The character to be checked
- * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * @c: character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+else
+return (0);
 }
-b
