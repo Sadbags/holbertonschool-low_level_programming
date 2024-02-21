adce0a34aed14fb9302212912e36ee7e2bd4b513
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-21 13:17:18 -0400

    3 is lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,11 @@
 #include "main.h"
+
 /**
- * main - check the code
- * Description: checks lower case characters
- * Return: Always 0 (Success)
+ * _islower - checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-int main(void)
+int _islower(int c)
 {
-int r;
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+    return (c >= 'a' && c <= 'z');
 }
