38c901485c6f2da98e752f97bf687385e61ae310
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-21 14:41:56 -0400

    7 print

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Description: Funtion that prints the last digit of a number
+ * return: Always 0
+ */
+int main(void)
+{
+print_last_digit(98);
+print_last_digit(0);
+print_last_digit(-402);
+return (0);
+}
