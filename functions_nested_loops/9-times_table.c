9ff0222b661064c56384ec30d1fcb9513b6e19da
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-21 16:05:52 -0400

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,39 +2,35 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- *
- * Return: void
  */
 void times_table(void)
 {
-int i, j, result;
+	int i, j, k;
 
-for (i = 0; i < 10; i++)
-{
- _putchar('0');
-_putchar(',');
-_putchar(' ');
-for (j = 1; j < 10; j++)
-{
-result = i * j;
-if (result < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(result + '0');
-}
-else
-{
-_putchar(' ');
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-}
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
