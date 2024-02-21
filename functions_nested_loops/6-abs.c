9736f9ee0d7f9f44737fdc3a6d9726fa83f50a9b
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-21 13:52:54 -0400

    6 abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * write a funtion that computes the absolute value of an integer
+ * @n: The integer to be printed
+ * 
+ * Return: void
+ */
+int _abs(int n)
+{
+if (n <= 0)
+return (n);
+else 
+return (-n);
+}
