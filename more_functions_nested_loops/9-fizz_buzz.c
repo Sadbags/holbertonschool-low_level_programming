9120d4642822c31eb0ff66340b16d6db183bf3f7
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-22 13:15:25 -0400

    9 fizz buzz2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * fizz_buzz - prints the numbers from 1 to 100,
  * but for multiples of 3, print Fizz
  * instead of the number, and for the multiples of 5, print Buzz
  * instead of the number, and for multiples of both 3 and 5, print FizzBuzz
- *
  * Return: void
  */
 void fizz_buzz(void)
