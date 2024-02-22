f58a5b6bb6458b1ccd35bc79035aaedc8749393c
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-22 11:51:04 -0400

    9 fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,8 +3,7 @@
  * fizz_buzz - prints the numbers from 1 to 100,
  * but for multiples of 3, print Fizz
  * instead of the number, and for the multiples of 5, print Buzz
- * instead of the number,
- * and for multiples of both 3 and 5, print FizzBuzz
+ * instead of the number, and for multiples of both 3 and 5, print FizzBuzz
  *
  * Return: void
  */
