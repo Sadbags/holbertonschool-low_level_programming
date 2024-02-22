94a60465e4a167fa90a0e98732f9832376c7738b
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-22 14:05:34 -0400

    9 fizz buzz2

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,28 @@
 void fizz_buzz(void)
 {
 int i;
-
 for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
+if (i % 3 == 0 && i % 5 == 0)
+{
 printf("FizzBuzz");
+}
 else if (i % 3 == 0)
+{
 printf("Fizz");
+}
 else if (i % 5 == 0)
+{
 printf("Buzz");
+}
 else
+{
 printf("%d", i);
-
+}
 if (i < 100)
+{
 printf(" ");
+}
 }
 printf("\n");
 }
