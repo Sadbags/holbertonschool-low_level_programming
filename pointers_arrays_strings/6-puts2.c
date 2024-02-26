a9a289201579279e569089a320f98a1e04bf2eb9
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-26 14:47:52 -0400

    6 puts update1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * puts2 - prints one char out of 2 of a string
  * @str: The string containing characters
@@ -10,8 +9,6 @@ int index = 0, len = 0
 while (str[index++])
 len++;
 for (index = 0; index < len; index += 2)
-{
 _putchar(str[index]);
-}
 _putchar('\n');
 }
