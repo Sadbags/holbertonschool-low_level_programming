931fe579b907140fd5c4926e7e459a3753284c95
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-26 14:49:42 -0400

    6 puts update1

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,10 +5,14 @@
  */
 void puts2(char *str);
 {
-int index = 0, len = 0
-while (str[index++])
-len++;
-for (index = 0; index < len; index += 2)
-_putchar(str[index]);
+int i = 0;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
+}
 _putchar('\n');
 }
