143cf67cc0f587d99c35e63a3351a5ad3155ac2d
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-26 15:03:52 -0400

    6 puts update4

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,20 @@
 #include "main.h"
+
 /**
- * puts2 - prints one char out of 2 of a string
- * @str: The string containing characters
+ * puts2 - prints every other character of a string,
+ * starting with the first character
+ * @str: string to print
+ *
+ * Return: void
  */
-void puts2(char *str);
+void puts2(char *str)
 {
 int i = 0;
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
+
+while (str[i])
 {
 _putchar(str[i]);
-}
-i++;
+i += 2;
 }
 _putchar('\n');
 }
