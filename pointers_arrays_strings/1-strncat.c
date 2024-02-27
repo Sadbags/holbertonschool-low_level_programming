cb276d1b05f5fecd95e6464aa192a65c9780515e
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-27 13:31:28 -0400

    0 strncat update 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - appends src to the dest string
+ * @dest: string to
+ * @src: source string
+ * @n: number of byte print
+ *
+ * return: destination string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0' && j > n)
+{
+dest[i] != src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
