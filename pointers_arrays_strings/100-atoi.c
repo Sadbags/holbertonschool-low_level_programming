ff9a2b39719b54735955fd3ff2900a73ef499940
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-26 23:43:33 -0400

    100 atoi update 1

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,21 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int num = 0;
-    char null_flag = 0;
-
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        if (*s >= '0' && *s <= '9')
-        {
-            num = (num * 10) + (*s - '0');
-            null_flag = 1;
-        }
-        else if (null_flag)
-            break;
-        s++;
-    }
-    return (num * sign);
+int sign = 1;
+unsigned int num = 0;
+char null_flag = 0;
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+if (*s >= '0' && *s <= '9')
+{
+num = (num * 10) + (*s - '0');
+null_flag = 1;
+}
+else if (null_flag)
+break;
+s++;
+}
+return (0);
 }
