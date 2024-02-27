6797ec5ff09116663d7d105787ede7570904e3ff
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-27 16:48:48 -0400

    6 cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes the first letter of each word in a string
+ * @n: input string
+ *
+ * Return: pointer to the modified string
+*/
+char *cap_string(char *n)
+{
+int i;
+if (n[0] >= 'a' && n[0] <= 'z')
+{
+n[0] = toupper(n[0]);
+}
+for (i = 1; n[i] != '\0'; i++)
+{
+if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n' || n[i] == '\r' ||
+n[i] == '\f' || n[i] == '\v')
+{
+if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
+{
+n[i + 1] = toupper(n[i + 1]);
+}
+}
+}
+return (n);
+}
