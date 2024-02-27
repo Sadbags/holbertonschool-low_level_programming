844359d3a8eea8e4a97730d97196cb6ec9d161b8
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-26 23:34:24 -0400

    9 strcpy update 1

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,15 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i + 0;
-while (src[i] != '\0')
+int len , i;
+len = 0;
+while (src[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i++)
 {
 dest[i] = src[i];
-i++;
 }
 dest[i] = '\0';
 return (dest);
