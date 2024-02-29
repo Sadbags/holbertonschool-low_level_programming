9b120cd4cc2ca284ed7046a601d31c0689bd6017
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-29 10:29:23 -0400

    0 memset update 2

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -4,13 +4,13 @@
  * _memset - fill a block of memory with a constant byte
  * @s: pointer to the memory area to be filled
  * @b: value to be set
- * @n: number of byte to be set by value
+ * @n: number of bytes to be set by value
  * Return: a pointer to the memory area s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 s[i] = b;
 }
