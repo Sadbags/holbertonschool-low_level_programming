61885349617f15ade0fe7a21e8a08cf6d484af1e
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-29 11:19:44 -0400

    1 memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: final destination
+ * @src: memory area
+ * @n: memory area
+ * Return: returns a pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (unsigned int i = 0; i < n; i++)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
