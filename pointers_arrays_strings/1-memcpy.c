7c8a9dfed9f9fc2158795953a3eb2e7f4400bb0c
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-29 11:21:48 -0400

    1 memcpy update 2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
-for (unsigned int i = 0; i < n; i--)
+for (unsigned int i = 0; i < n; i++)
 {
 dest[i] = src[i];
-i++;
+i--;
 }
 return (dest);
 }
