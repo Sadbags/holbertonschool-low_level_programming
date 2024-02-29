4a4682cc3672bd4696a9317da34e7e053e3bcbdb
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-02-29 14:36:10 -0400

    3 strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strspn - write a funtion that gets the lenght of a prefix
- * @s: string to check
+ * _strspn - write a funtion that gets the lenght of a prefix substring
+ * @s: string to evaluate
  * @accept: string containing the list of character to match in s
  *
  * Return: the number of bytes in the initial segment
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int i, j;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != ; j++)
+for (j = 0; accept[j] != s[i]; j++)
 if (accept[j] == '\0')
 return (i);
 }
