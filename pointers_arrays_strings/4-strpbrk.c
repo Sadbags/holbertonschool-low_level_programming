01b24dd73f4d2731b90f6ecafc1eb12e3a83407b
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-02-29 22:08:05 -0400

    4 strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - funtion that searches a string for any of a set of bytes
+ * @s: string to be searched
+ * @accept: string containing characters to match
+ * return: string s that matches one of the bytescin accept
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+}
+}
+return (0);
+}
