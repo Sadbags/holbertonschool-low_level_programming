df99685e409a57a9e73581f04269286197507dd4
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-03-04 10:39:47 -0400

    0 puts recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string followed by a new line
+ * @s: the string
+ * Return: 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\n')
+{
+write("\n");
+return;
+}
+}
