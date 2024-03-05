1a428364df260b14de14bedb15fd001dcc1dd08f
Bryan Garcia <bags@bryans-air.lan>
2024-03-05 11:19:18 -0400

    1 args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argv: command line arguments
+ * @argc: command line arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d", argc);
+return (0);
+}
