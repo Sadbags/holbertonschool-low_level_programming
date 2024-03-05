031d79849c4533b189073e2490bcc497e7f7ae2d
Bryan Garcia <bags@bryans-air.lan>
2024-03-05 11:56:52 -0400

    1 args update 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - main function
- * @argv: command line arguments
- * @argc: command line arguments
+ * main - prints all arguments it receives.
+ * @argc: There number of arguments supplied to the program.
+ * @argv: An array of pointers to the argument
+ *
  * Return: 0
 */
 int main(int argc, char *argv[])
 {
 int i;
-argv++;
-argc--;
 for (i = 0; i < argc - 1; i++)
-{
-printf("%dn", i);
+;
+printf("%d", i);
 return (0);
-}
 }
