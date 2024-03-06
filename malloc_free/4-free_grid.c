25e5337686f386c7bf3cca49788b76f609484051
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-03-06 14:35:36 -0400

    4 free grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,7 +5,6 @@
  * free_grid - frees a 2d grid
  * @grid: pointer to pointer array
  * @height: size of the grid
- *
  * Return: the number of free cells in the grid
 */
 void free_grid(int **grid, int height)
