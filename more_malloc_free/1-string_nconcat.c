a6db0c04f87bb3a5f3b29261f9ee6f20f41522e8
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-03-07 10:32:25 -0400

    1 string nconcat update 2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * string_nconcat - Concatenate a string with another string
- * @s1: the string to concatenate with the delimiter string
- * @s2: the string to concatenate with the delimiter string
- * @n: the number of characters to concatenate with the delimiter string
+ * string_nconcat - concatenates two strings
+ * @s1: first tring in the concatenation
+ * @s2: string from whic to copy @n bytes
+ * @n: desired amount of bytes from @s2
  *
- * Return: the concatenated string with the specified delimiter
-*/
+ * Return: pointer to a newly allocated space in memory, NULL if
+ * the function fails
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, j = 0, k = 0, l = 0;
-char *str;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[i])
-i++;
-while (s2[k])
-k++;
-if (n >= k)
-l = i + k;
-else
-l = i + n;
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
-return (NULL);
-k = 0;
-while (j < l)
-{
-if (j <= i)
-str[j] = s1[j];
-if (j >= l)
-{
-str[j] = s2[j];
-k++;
-}
-j++;
-}
-str[j] = '\0';
-return (str);
+	unsigned int i, m_idx = 0;
+	char *m;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++, m_idx++)
+		;
+	for (i = 0; s2[i] != '\0' && i < n; i++, m_idx++)
+		;
+
+	m = malloc(sizeof(char) * m_idx + 1);
+	if (m == NULL)
+		return (NULL);
+	m[m_idx] = '\0';
+
+	for (m_idx = 0, i = 0; s1[i] != '\0'; i++, m_idx++)
+		m[m_idx] = s1[i];
+	for (i = 0; s2[i] != '\0' && i < n; i++, m_idx++)
+		m[m_idx] = s2[i];
+
+	return (m);
 }
