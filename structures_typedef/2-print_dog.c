72d7ceaed2b30af946f39a1b8c91553c604e7e71
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-03-11 14:38:34 -0400

    2 print dog update 4

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,18 +8,16 @@
 */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-if (d->name == NULL)
-printf("name: (nil)\n");
-else
+if (d != NULL)
+{
+if (d->name != NULL)
 printf("name: %s\n", d->name);
-if (d->age < 0)
-printf("age: (nil)\n");
 else
-printf("age: %f\n", d->age);
-if (d->owner == NULL)
-printf("owner: (nil)\n");
-else
+printf("name: (nil)\n");
+printf("Age: %.6f\n", d->age);
+if (d->owner != NULL)
 printf("owner: %s\n", d->owner);
+else
+printf("owner: (nil)\n");
+}
 }
