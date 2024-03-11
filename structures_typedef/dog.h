b0105e8ef622b81dccc2333f01872a945154ef19
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-03-11 12:10:00 -0400

    dog h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,15 @@
-struct dog{
+#ifndef DOG
+#define DOG
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - dog description
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner of dog
+*/
+struct dog
+{
 char *name;
 float age;
 char *owner;
