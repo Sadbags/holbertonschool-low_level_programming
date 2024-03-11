591c211ff09e3383244feaf4fd9c5dbbf0c7073e
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-03-11 15:05:43 -0400

    2 print dog update 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,12 +11,12 @@ void print_dog(struct dog *d)
 if (d != NULL)
 {
 if (d->name != NULL)
-printf("name: %s\n", d->name);
+printf("name: %s\n", d.name);
 else
 printf("name: (nil)\n");
-printf("Age: %.6f\n", d->age);
+printf("Age: %.6f\n", d.age);
 if (d->owner != NULL)
-printf("owner: %s\n", d->owner);
+printf("owner: %s\n", d.owner);
 else
 printf("owner: (nil)\n");
 }
