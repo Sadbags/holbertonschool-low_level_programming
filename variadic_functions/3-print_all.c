5139095dd5d0e7466c88115b2ef96b1fad4f6182
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-03-14 11:15:26 -0400

    3 print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function prints anything
+ * @format: data types
+ * Return: nothing:
+*/
+void print_all(const char * const format, ...)
+{
+int count2 = 0;
+va_list printList;
+char *string, *comma = "";
+
+if (format == NULL)
+{
+printf("\n");
+return;
+}
+va_start(printList, format);
+while (format[count2] != '\0')
+{
+switch (format[count2])
+{
+case 'c':
+printf("%s%c", comma, va_arg(printList, int));
+comma = ", ";
+break;
+case 'i':
+printf("%s%d", comma, va_arg(printList, int));
+comma = ", ";
+break;
+case 'f':
+printf("%s%f", comma, va_arg(printList, double));
+comma = ", ";
+break;
+case's':
+string = va_arg(printList, char *);
+if (string == NULL)
+string = "(nil)";
+printf("%s%s", comma, string);
+comma = ", ";
+break;
+}
+count2++;
+}
+printf("\n");
+va_end(printList);
+}
