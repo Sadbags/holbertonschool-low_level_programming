b0fe7b3867b5e0eeb919444f8a409381a8a54aeb
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-03-25 12:52:39 -0400

    100 first

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void myStartupfun(void) __attribute__ ((constructor));
 */
 void myStartupFun(void)
 {
-printf("You're bea! and yet, you must allow,\n");
+printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
