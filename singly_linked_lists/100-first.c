1de8637cc108b59285c2797d32d58290792b5c72
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-03-25 12:50:38 -0400

    100 first

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * myStartupFun - Apply constructor attribute to mystartupfun
+*/
+void myStartupfun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+*/
+void myStartupFun(void)
+{
+printf("You're bea! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
